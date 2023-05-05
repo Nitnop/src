b7efc8515d0b0fd0899a7a6c688244045ce9be93
Nitnop <64380086+Nitnop@users.noreply.github.com>
2023-05-05 11:42:28 -0600

    Avanti!

---
+++ UserPromps.java
@@ -0,0 +1,23 @@
+public class UserPromps {
+    
+    static void getUserprompts(Boolean manager){
+        System.out.println("Welcome to store invintory management helper, how may I help you today?");
+        System.out.println("You can add a new movie to invintory by entering 1");
+        System.out.println("You can remove a movie from invintory be entering 2");
+        System.out.println("Sort and Print current invintory by entering 3");
+        System.out.println("To look up a movie by name enter 4");
+        System.out.println("To look up a movie by actor enter 5");
+        System.out.println("To look up a movie by genre enter 6");
+        System.out.println("To look up a movie by year enter 7");
+        System.out.println("You can quit by entering 8");
+    }
+
+    static void getUserprompts(){
+        System.out.println("Welcome to store invintory management helper, how may I help you today?");
+        System.out.println("To look up a movie by name enter 1");
+        System.out.println("To look up a movie by actor enter 2");
+        System.out.println("To look up a movie by genre enter 3");
+        System.out.println("To look up a movie by year enter 4");
+        System.out.println("You can quit by entering 5");
+    }
+}
